400e80ce99420a1f17e8d6f4bd5c95cb02599d2d
artischic <artischic101@gmail.com>
2025-10-15 19:24:46 +0530

    Create addTwoNos.cpp

---
+++ addTwoNos.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n1, n2, sum = 0;
+  cout << "Enter first num: ";
+  cin >> n1;
+
+  cout << "Enter second nun: ";
+  cin >> n2;
+
+  sum = n1 + n2;
+  cout << "Sum is: " << sum << endl;
+
+  return 0;
+}
+  
